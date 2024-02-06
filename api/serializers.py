0124dd18ae311b42a800a5476ba8cbcbc9c28adc
rafinder1 <rafpro33@gmail.com>
2024-02-06 13:36:52 +0100

    created Movie Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,16 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from api.models import Movie
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['title', 'description', 'after_premiere']
