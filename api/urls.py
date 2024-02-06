a70c582e862099312d53936f75b27ffd0fe8404f
rafinder1 <rafpro33@gmail.com>
2024-02-06 14:07:42 +0100

    added basename in router movies

--- api/urls.py
+++ api/urls.py
@@ -22,7 +22,7 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'movies', views.MovieViewSet)
+router.register(r'movies', views.MovieViewSet, basename='movie')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
