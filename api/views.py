535b6deadd0279a985af6f5da613e7b814c56c25
rafinder1 <rafpro33@gmail.com>
2024-02-06 13:38:28 +0100

    created Movie View Set

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import User
 from rest_framework import permissions, viewsets
 
-from api.serializers import UserSerializer
+from api.models import Movie
+from api.serializers import UserSerializer, MovieSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
