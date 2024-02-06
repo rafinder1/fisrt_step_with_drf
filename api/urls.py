89b60f58f17e5cf4f471efc9fc221ed9d688a119
rafinder1 <rafpro33@gmail.com>
2024-02-06 13:38:49 +0100

    registert movies url

--- api/urls.py
+++ api/urls.py
@@ -22,6 +22,7 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'movies', views.MovieViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
