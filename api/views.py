61619f921d35090a477732d3fd608ae5d2f1b0d6
rafinder1 <rafpro33@gmail.com>
2024-02-06 13:24:05 +0100

    deleted coments from user view set

--- api/views.py
+++ api/views.py
@@ -5,9 +5,6 @@ from api.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
